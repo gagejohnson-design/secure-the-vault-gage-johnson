97bcc3e5eb60fddd74676eafe83b5be22c49fa6f
Gage Johnson <gagejohnson117@gmail.com>
2026-01-28 17:10:20 -0600

    Completed Assignment logic

---
+++ index.js
@@ -0,0 +1,17 @@
+let code1;
+
+code1 = 7 + 3;
+
+let code2 = 110 - 140 / 2;
+
+let code3 = (83 % 20) + 36;
+
+let message = "The vault has been secured. The combination is:";
+
+let codeA = code1 + " - " + code2 + " - " + code3;
+
+let codeB = `${code1}-${code2}-${code3}`;
+
+console.log(message);
+console.log(codeA);
+console.log(codeB);
